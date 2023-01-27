55623279c2657295a9c93b56d37496eaaba490bb
ManeDM <mane657@hotmail.com.com>
2023-01-26 21:39:55 -0500

    Fith commit

--- src/app/Shared/header/header.component.ts
+++ src/app/Shared/header/header.component.ts
@@ -1,10 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.scss']
 })
-export class HeaderComponent {
+export class HeaderComponent implements OnInit {
 
+  ngOnInit(): void {
+    this.burguer() 
+  }
+
+  burguer(){
+  const btn =  document.querySelector('#menu-btn');
+  const menu = document.querySelector('#sidemenu');
+  btn?.addEventListener('click', i =>{
+    menu?.classList.toggle("menu-expanded");
+    menu?.classList.toggle("menu-collapsed")
+
+    document.querySelector('body')?.classList.toggle('body-expanded');
+    
+  });
+  }
+  
 }
